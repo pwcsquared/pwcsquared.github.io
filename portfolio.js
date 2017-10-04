a59e8256bfe9d5af733861bf150da73e0b85dfd1
Patrick Carr <pwcsquared@gmail.com>
2017-10-04 16:27:41 -0500

    added some project images, altered some project cards, minor style tweaks

--- portfolio.js
+++ portfolio.js
@@ -7,9 +7,13 @@ $(document).ready(function(){
 			scrollTop: $(navTo).offset().top
 		}, 500);
 
-		//change focus to first link inside navTo div
-		if($(navTo).find("a")[0]){
-			$(navTo).find("a")[0].focus();
+
+		//change focus to first link inside navTo div if keyboard was used
+		if(e.originalEvent.screenX === 0 && e.originalEvent.screenY === 0){
+			//click was triggered by pressing enter on focused element
+			if($(navTo).find("a")[0]){
+				$(navTo).find("a")[0].focus();
+			}
 		}
 	});
 });
